3808b796c7d9cac53a58b9a1d3a6d50ac8c916b9
Peter Nielsen <peterflemmingnielsen@gmail.com>
2019-08-19 19:43:51 +0200

    Added a lot of components Added a lot of fonts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PlayerComponent} from "./player/player.component";
+import {InformationComponent} from "./information/information.component";
+import {CharactersComponent} from "./characters/characters.component";
+import {SessionComponent} from "./session/session.component";
+import {CampaignComponent} from "./campaign/campaign.component";
+import {AccountComponent} from "./account/account.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path': '', component: InformationComponent},
+  {'path': 'player', component: PlayerComponent},
+  {'path': 'character', component: CharactersComponent},
+  {'path': 'session', component: SessionComponent},
+  {'path': 'campaign', component: CampaignComponent},
+  {'path': 'account', component: AccountComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AlertModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { CharactersComponent } from './characters/characters.component';
+import { SessionComponent } from './session/session.component';
+import { CampaignComponent } from './campaign/campaign.component';
+import { AccountComponent } from './account/account.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    CharactersComponent,
+    SessionComponent,
+    CampaignComponent,
+    AccountComponent,
+    InformationComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
